599bfa9cac1ea81167424c9446a4b1466ae4fd94
huilin <lihuilin023@gmail.com>
2022-05-12 22:33:27 +0200

    commit

--- UNIOA/optimizer_running.py
+++ UNIOA/optimizer_running.py
@@ -5,53 +5,13 @@ from .algs import *
 import sys, os
 from pydoc import locate
 
-def optimizer_running(problems, instances, dimensions, num_runs, paras_set, optimizer_name, output_name):
-    Which_alg = output_name.split("_")[1]
-    folder_name = Which_alg + '_folder'
-    data_name = output_name
-    st = time.time()
-    print('--->   ' + output_name + ' is optmizting', flush=True)
-    logger = ioh.logger.Analyzer(folder_name='DataFiles/'+folder_name+'/'+data_name, algorithm_name=output_name)
-    for p_id in problems:
-        print('P-id={}'.format(p_id), end=":", flush=True)
-        for d in dimensions:
-            print('d-{}'.format(d), end=":", flush=True)
-            for i_id in instances:
-                print('{}'.format(i_id), end="", flush=True)
-                func = ioh.get_problem(fid=p_id, dim=d, iid=i_id)
-                print('', end='(', flush=True)
-                for rep in range(num_runs):
-                    #func.attach_logger(logger)
-                    print('{}'.format(rep), end="", flush=True)
-                    func.attach_logger(logger)
-                    opt = eval(optimizer_name)(func, paras_set)
-                    opt()
-                    func.reset()
-                print('', end=')', flush=True)
-
-    print('costs', round((time.time() - st) / 60, 2), 'minutes')
-    with open("DataFiles/runningtime_" + Which_alg + ".txt", "a+") as text_file:
-        date = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-        runningtime = round((time.time() - st) / 60, 2)
-        print("{} | {}: {} minutes.".format(date, output_name, runningtime), file=text_file)
-
-
-#
-# def optimizer_running(problems, instances, dimensions, num_runs, paras_set, optimizer_name):
-#     t = 0
-#     optimizer_name_temp = optimizer_name
-#     UNIOA_algs = ['BA_UNIOA', 'CSA_UNIOA', 'MFO_UNIOA', 'PSO_UNIOA', 'GOA_UNIOA', 'MBO_UNIOA', 'BOA_UNIOA']
-#     if optimizer_name not in UNIOA_algs:
-#         t = 1
-#         file_name = os.path.basename(sys.argv[0])[:-2]
-#         your_optimizer_name = locate(file_name + optimizer_name)
-#
-#
-#     folder_name = optimizer_name_temp + '_folder'
-#     data_name = optimizer_name_temp
+# def optimizer_running(problems, instances, dimensions, num_runs, paras_set, optimizer_name, output_name):
+#     Which_alg = output_name.split("_")[1]
+#     folder_name = Which_alg + '_folder'
+#     data_name = output_name
 #     st = time.time()
-#     print('----- ' + optimizer_name_temp + ' is optmizting your problem -----', flush=True)
-#     logger = ioh.logger.Analyzer(folder_name='DataFiles/'+folder_name+'/'+data_name, algorithm_name=optimizer_name_temp)
+#     print('--->   ' + output_name + ' is optmizting', flush=True)
+#     logger = ioh.logger.Analyzer(folder_name='DataFiles/'+folder_name+'/'+data_name, algorithm_name=output_name)
 #     for p_id in problems:
 #         print('P-id={}'.format(p_id), end=":", flush=True)
 #         for d in dimensions:
@@ -59,24 +19,64 @@ def optimizer_running(problems, instances, dimensions, num_runs, paras_set, opti
 #             for i_id in instances:
 #                 print('{}'.format(i_id), end="", flush=True)
 #                 func = ioh.get_problem(fid=p_id, dim=d, iid=i_id)
-#                 # func.attach_logger(logger)
 #                 print('', end='(', flush=True)
 #                 for rep in range(num_runs):
-#                     func.attach_logger(logger)
+#                     #func.attach_logger(logger)
 #                     print('{}'.format(rep), end="", flush=True)
-#                     if t == 1 :
-#                         opt = your_optimizer_name(func, paras_set)
-#                     else:
-#                         opt = eval(optimizer_name)(func, paras_set)
+#                     func.attach_logger(logger)
+#                     opt = eval(optimizer_name)(func, paras_set)
 #                     opt()
 #                     func.reset()
 #                 print('', end=')', flush=True)
 #
-#
 #     print('costs', round((time.time() - st) / 60, 2), 'minutes')
-#     with open("DataFiles/runningtime.txt", "a+") as text_file:
+#     with open("DataFiles/runningtime_" + Which_alg + ".txt", "a+") as text_file:
 #         date = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 #         runningtime = round((time.time() - st) / 60, 2)
-#         print("{} | {}: {} minutes.".format(date, optimizer_name, runningtime), file=text_file)
+#         print("{} | {}: {} minutes.".format(date, output_name, runningtime), file=text_file)
 #
-#
+
+
+def optimizer_running(problems, instances, dimensions, num_runs, paras_set, optimizer_name):
+    t = 0
+    optimizer_name_temp = optimizer_name
+    UNIOA_algs = ['BA_UNIOA', 'CSA_UNIOA', 'MFO_UNIOA', 'PSO_UNIOA', 'GOA_UNIOA', 'MBO_UNIOA', 'BOA_UNIOA']
+    if optimizer_name not in UNIOA_algs:
+        t = 1
+        file_name = os.path.basename(sys.argv[0])[:-2]
+        your_optimizer_name = locate(file_name + optimizer_name)
+
+
+    folder_name = optimizer_name_temp + '_folder'
+    data_name = optimizer_name_temp
+    st = time.time()
+    print('----- ' + optimizer_name_temp + ' is optmizting your problem -----', flush=True)
+    logger = ioh.logger.Analyzer(folder_name='DataFiles/'+folder_name+'/'+data_name, algorithm_name=optimizer_name_temp)
+    for p_id in problems:
+        print('P-id={}'.format(p_id), end=":", flush=True)
+        for d in dimensions:
+            print('d-{}'.format(d), end=":", flush=True)
+            for i_id in instances:
+                print('{}'.format(i_id), end="", flush=True)
+                func = ioh.get_problem(fid=p_id, dim=d, iid=i_id)
+                # func.attach_logger(logger)
+                print('', end='(', flush=True)
+                for rep in range(num_runs):
+                    func.attach_logger(logger)
+                    print('{}'.format(rep), end="", flush=True)
+                    if t == 1 :
+                        opt = your_optimizer_name(func, paras_set)
+                    else:
+                        opt = eval(optimizer_name)(func, paras_set)
+                    opt()
+                    func.reset()
+                print('', end=')', flush=True)
+
+
+    print('costs', round((time.time() - st) / 60, 2), 'minutes')
+    with open("DataFiles/runningtime.txt", "a+") as text_file:
+        date = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        runningtime = round((time.time() - st) / 60, 2)
+        print("{} | {}: {} minutes.".format(date, optimizer_name, runningtime), file=text_file)
+
+
--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 from UNIOA import *
 
 # main optimization on x
-def your_Opt_X(old_x, y, x_g, z1, z2, w) :
-    new_x = ( old_x * z1 + y * z2 ) * w + x_g
+def your_Opt_X(old_x, y, x_ip, z, w) :
+    new_x = ( old_x * z - y ) * w + x_ip
     return new_x
 Opt_X.your = your_Opt_X
 
@@ -15,8 +15,8 @@ def your_Opt_Delta_Y(old_y, w):
 Opt_Delta_Y.your = your_Opt_Delta_Y
 
 ## x-relative step-size
-Init_Delta_X.your = Init_Delta_X.Global_best
-Opt_Delta_X.your = Opt_Delta_X.Global_best
+Init_Delta_X.your = Init_Delta_X.Personal_best
+Opt_Delta_X.your = Opt_Delta_X.Personal_best
 
 ## z-relative step-size
 def your_InitOpt_Delta_z(t, old_z, w):
@@ -51,19 +51,19 @@ class Your_Opt(NatureOpt):
         X = self.Init_X.Init_X(M=self.M, n=self.n, lb_x=self.lb_x, ub_x=self.ub_x)
         X_Fit = self.Evaluate_X(X=X)
         Y = self.Init_Delta_Y.interval_type(M=self.M, n=self.n, interval=[-1,1])
-        X_g, X_g_Fit = self.Init_Delta_X.your(new_X=X, new_X_Fit=X_Fit)
-        z1 = self.InitOpt_Delta_z.your[0](t=t, old_z=self.z_0, w=self.w1)
+        X_ip, X_ip_Fit = self.Init_Delta_X.your(new_X=X, new_X_Fit=X_Fit)
+        z = self.InitOpt_Delta_z.your(t=t, old_z=self.z_0, w=self.w1)
 
         # Optimizing
         while not self.stop:
             new_Y = self.Opt_Delta_Y.your(old_y=Y, w=self.w2)
-            temp_X = self.Opt_X.your(old_x=X, y=new_Y, x_g=X_g, z1=z1, w=self.w3)
+            temp_X = self.Opt_X.your(old_x=X, y=new_Y, x_ip=X_ip, z=z, w=self.w3)
             temp_X_Fit = self.Evaluate_X(X=X)
             new_X, new_X_Fit = self.Selection.your(temp_X=temp_X, temp_X_Fit=temp_X_Fit, old_X=X, old_X_Fit=X_Fit)
 
             t = t + 1
-            z = self.InitOpt_Delta_z.your[0](t=t, old_z=z, w=self.w1)
-            X_g, X_g_Fit = self.Opt_Delta_X.your(new_X=new_X, new_X_Fit=new_X_Fit, old_X_g=X_g, old_X_g_Fit=X_g_Fit)
+            z = self.InitOpt_Delta_z.your(t=t, old_z=z, w=self.w1)
+            X_ip, X_ip_Fit = self.Opt_Delta_X.your(new_X=new_X, new_X_Fit=new_X_Fit, old_X_p=X_ip, old_X_p_Fit=X_ip_Fit)
             X = new_X
             X_Fit = new_X_Fit
 
@@ -71,7 +71,7 @@ class Your_Opt(NatureOpt):
 if __name__ == '__main__':
     # This is for testing customized optimizer
     #######################
-    Algs = ['Your_Opt', 'PSO_UNIOA']
+    Algs = ['Your_Opt', 'BA_UNIOA']
     problems = [1, 5, 10, 22]
     instances = [5]
     dimensions = [5]
@@ -81,6 +81,6 @@ if __name__ == '__main__':
                                 'w1': 0.8,
                                 'w2': 0.6,
                                 'w3': 0.7 },
-                  'PSO_UNIOA': {},
+                  'BA_UNIOA': {},
                  }
     comparison.comparing(Algs, problems, instances, dimensions, num_runs, paras_sets)
